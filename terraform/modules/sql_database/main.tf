5e90ca083b5ebe35dda1d8fae65702df93175dee
Avinash.maddeshiya <avinashmaddheshiya24@gmail.com>
2026-03-27 04:28:24 -0700

    modules created for sql server and database.

--- terraform/modules/sql_database/main.tf
+++ terraform/modules/sql_database/main.tf
@@ -2,4 +2,8 @@ resource "azurerm_mssql_database" "db" {
     name      = var.db_config.db_name
     server_id = var.db_config.server_id
     sku_name  = var.db_config.sku
+
+    lifecycle {
+        prevent_destroy = false
+    }
 }
